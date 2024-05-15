31b13235c0aed6758ed30559fa20ba784817dc8d
NikoVilla <nikovillanueva7@gmail.com>
2024-05-14 21:57:22 -0400

    eliminacion de lara y php

--- webapp/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route; #(defecto)
-#use IlluminateSupportFacadesRoute; #(ejemplo no funciona)
-
-Route::get('/', function () {
-  $name = "Nicolas Villanueva";
-  return view('welcome', ['name' => $name]);
-});
